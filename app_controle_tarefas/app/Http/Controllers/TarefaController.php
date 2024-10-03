b6803fa1ff14a4c3d88d20f3a7cbed8821094b53
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-10-02 23:05:38 -0300

    ✨initial commit

---
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    // public function __construct() {
+    //     $this->middleware('auth');
+    // }
+    public function index()
+    {
+        if (auth()->check()) {
+            $id = auth()->user()->id;
+            $name = auth()->user()->name;
+            $email = auth()->user()->email;
+
+            return "id: $id, name: $name, email: $email";
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}
