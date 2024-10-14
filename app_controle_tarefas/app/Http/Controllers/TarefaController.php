cf4d19a8c55dc1fa35b52a7df9a9d49b5d169b76
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-10-14 01:48:47 -0300

    ✨feat: Atualizando registros de tarefas

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -59,7 +59,7 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        //
+        return view('tarefa.edit', ['tarefa' => $tarefa]);
     }
 
     /**
@@ -67,7 +67,8 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        //
+        $tarefa->update($request->all());
+        return redirect()->route('tarefa.show', $tarefa->id);
     }
 
     /**
