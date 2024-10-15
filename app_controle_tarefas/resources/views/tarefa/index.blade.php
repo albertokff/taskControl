a219efbb9cc939002a29bd2c70eb004a55200f38
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-10-14 21:21:12 -0300

    ✨feat: Melhorando a Navegação

--- app_controle_tarefas/resources/views/tarefa/index.blade.php
+++ app_controle_tarefas/resources/views/tarefa/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Tarefas</div>
+                <div class="card-header">Tarefas<a href="{{ route('tarefa.create') }}" class="btn btn-success float-end">Adicionar</a></div>
 
                 <div class="card-body">
                     <table class="table table-striped">
