7979ff132102d5620cca950a61075f035cb4091a
Diogo  Alberto <kirchoffalberto@gmail.com>
2024-10-14 21:04:38 -0300

    ✨feat: Validando se a Tarefa pertence ao usuário antes de habilitar a edição

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -59,6 +59,11 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
+        $user_id = auth()->user()->id;
+        if ($tarefa->user_id != $user_id) {
+            return view('acesso-negado');
+        }
+
         return view('tarefa.edit', ['tarefa' => $tarefa]);
     }
 
@@ -67,6 +72,11 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+        $user_id = auth()->user()->id;
+        if ($tarefa->user_id != $user_id) {
+            return view('acesso-negado');
+        }
+        
         $tarefa->update($request->all());
         return redirect()->route('tarefa.show', $tarefa->id);
     }
